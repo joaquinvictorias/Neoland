39287bf32144918580e97c20c596a21688f89961
joaquinvictorias <joaquinvictoriasanmartin@gmail.com>
2025-02-18 20:02:56 +0100

    pruebaVercel

--- ecommerce-project/backend/app.js
+++ ecommerce-project/backend/app.js
@@ -18,5 +18,6 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/orders", orderRouter);
+app.use("/", (res) => res.send("Hola"))
 
 module.exports = app;
